d311860b8c0e6c9f6088104329b87925067a859d
ShinikumiAlex <144524709+ShinikumiAlex@users.noreply.github.com>
2025-02-19 09:21:05 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inf19._02._2025__2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mas = Input();
+            Output(mas);
+            Console.ReadKey();
+        }
+
+        static int[] Input()
+        {
+            int[] mas = new int[5];
+            for (int i = 0; i < mas.Length; i++)
+            {
+                Console.Write($"mas[{i}]: ");
+                mas[i] = int.Parse(Console.ReadLine());
+            }
+            return mas;
+        }
+        static void Output(int[] mas)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write($"{mas[i]} \t");
+            }
+        }
+
+
+    }
+}
+
