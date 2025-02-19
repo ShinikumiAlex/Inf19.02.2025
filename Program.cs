6c8f28aeb799ad07206268c9abcd9855e46c72ee
ShinikumiAlex <144524709+ShinikumiAlex@users.noreply.github.com>
2025-02-19 09:36:18 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,36 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Inf19._02._2025__2
+namespace Inf19._02._2025_3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int[] mas = Input();
-            Output(mas);
-            Console.ReadKey();
-        }
-
-        static int[] Input()
-        {
-            int[] mas = new int[5];
-            for (int i = 0; i < mas.Length; i++)
             {
-                Console.Write($"mas[{i}]: ");
-                mas[i] = int.Parse(Console.ReadLine());
-            }
-            return mas;
-        }
-        static void Output(int[] mas)
-        {
-            for (int i = 0; i < 5; i++)
-            {
-                Console.Write($"{mas[i]} \t");
+                int[] mas = new int[10];
+                Output(mas);
+                Console.ReadKey();
             }
         }
+        static int RandomValue()
+        {
+            return new Random().Next(-10, 11);
+        }
 
-
+        static void Output(int[] omas)
+        {
+            for (int i = 0; i < omas.Length; i++)
+            {
+                Random rnd = new Random();
+                omas[i] = rnd.Next(-10, 10);
+                Console.WriteLine($"mas[{i}] = {omas[i]} ");
+            }
+        }
     }
 }
-
